8fe3528673b6c95992ba159625dbbb0eb79a4c14
IvelinaCh <30810857+IvelinaCh@users.noreply.github.com>
2017-09-10 22:16:25 +0300

    итог
    исправлена карта

---
+++ js/javascript.js
@@ -0,0 +1,46 @@
+﻿var form_connect_open = document.querySelector(".open-btn");
+var connection_modal = document.querySelector(".connection");
+var close = connection_modal.querySelector(".close");
+
+var connect_form = connection_modal.querySelector("form");
+var u_name = connection_modal.querySelector("[name=u-name]");
+var y_mail = connection_modal.querySelector("[name=y-mail]");
+var comment = connection_modal.querySelector("[name=comment]");
+
+var storage_name = localStorage.getItem("u-name");
+var storage_mail = localStorage.getItem("y-mail");
+  
+  form_connect_open.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    connection_modal.classList.add("show-modal");
+	
+	if (storage_name) {
+	u_name.value = storage_name;
+	y_mail.focus();
+} else {
+	u_name.focus();
+	}
+  });
+  
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    connection_modal.classList.remove("show-modal");
+  });
+  
+  connect_form.addEventListener("submit", function (evt) {
+	if (!u_name.value || !y_mail.value || !comment.value) {
+	evt.preventDefault();	
+	console.log("Не все поля заполнены");
+} else {
+	localStorage.setItem("u_name", u_name.value);
+	localStorage.setItem("y_mail", y_mail.value);
+}
+  });
+  
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (connection_modal.classList.contains("show-modal")) {
+        connection_modal.classList.remove("show-modal");
+      }
+    }
+  });  
